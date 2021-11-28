2a583933bd737cbd05c82fcc89b62a5769adb34b
dogru1 <dogukandogru061@gmail.com>
2021-11-28 00:08:37 +0000

    vpc

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,8 +1,3 @@
- output = "aws_vpc" "main" {
-     value = aws_vpc.main.aws_vpc
-
-
-
- output "ID" {
-   value = aws_security_group.external_by_terraform.id
- }
+output "AZ" {
+  value = aws_subnet.public_subnet1.availability_zone 
+}
